c017d2d42077b382d502b5e59fb995ffcacd4e9b
Donatron <don.macarthur68@gmail.com>
2022-01-15 12:04:01 +0700

    email-opt-in: removed console.log statements

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,8 @@
 let rootUrl;
 
 if (process.env.NODE_ENV === 'production') {
-  console.log('Production...');
   rootUrl = 'https://sightwords-app.herokuapp.com';
 } else if (process.env.NODE_ENV === 'development') {
-  console.log('Dev...')
   rootUrl = 'http://localhost:5000'
 }
 
